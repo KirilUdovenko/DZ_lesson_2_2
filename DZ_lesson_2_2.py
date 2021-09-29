3fa30cc968c918be2938b543f64aa83164e32adf
KirilUdovenko <kiril.stuff1@gmail.com>
2021-09-29 16:17:53 +0300

    DZ complete

---
+++ DZ_lesson_2_2.py
@@ -0,0 +1,52 @@
+# value = 50
+# if value < 100:
+#     new_value = int(value / 2)
+#     print(new_value)
+# else:
+#     new_value = int(value * -1)
+#     print(new_value)
+
+#########################################
+
+# value = 50
+# if value < 100:
+#     new_value = 1
+#     print(new_value)
+# else:
+#     new_value = 0
+#     print(new_value)
+
+#########################################
+
+# value = 95
+# if value < 100:
+#     new_value = bool(value)
+#     print(new_value)
+# else:
+#     print(False)
+
+########################################
+
+# my_str = "hello, world!!!"
+# print(my_str.upper())
+
+#########################################
+
+# my_str = "HELLO, WORLD!!!"
+# print(my_str.lower())
+
+#########################################
+
+# my_str = "Hell"
+# if len(my_str) < 5:
+#     print(my_str * 2)
+# else:
+#     print(my_str)
+
+##########################################
+
+my_str = "qwer"
+if len(my_str) < 5:
+    print(my_str + my_str[::-1])
+else:
+    print(my_str)
