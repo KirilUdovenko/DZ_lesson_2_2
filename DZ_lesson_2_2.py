3a41cfad8277b3bf1a25e2294e14e5c5b35d1e38
KirilUdovenko <kiril.stuff1@gmail.com>
2021-10-05 23:35:34 +0300

    DZ complete.2.2.2

--- DZ_lesson_2_2.py
+++ DZ_lesson_2_2.py
@@ -1,34 +1,27 @@
-# value = 50
-# if value < 100:
-#     new_value = int(value / 2)
-#     print(new_value)
-# else:
-#     new_value = int(value * -1)
-#     print(new_value)
+# value = 106
+# new_value = value // 2 if value < 100 else - value
+# print(new_value)
 
 #########################################
 
 # value = 50
-# if value < 100:
-#     new_value = 1
-#     print(new_value)
-# else:
-#     new_value = 0
-#     print(new_value)
+# new_value = 1 if value < 100 else 0
+# print(new_value)
 
 #########################################
 
-# value = 95
-# if value < 100:
-#     new_value = bool(value)
-#     print(new_value)
-# else:
-#     print(False)
+# value = 15
+# new_value = bool(value) if value < 100 else False
+# print(new_value)
 
 ########################################
 
-# my_str = "hello, world!!!"
-# print(my_str.upper())
+my_str = "hello, world!!!"
+for symbol in my_str:
+    if symbol.isupper():
+        print(my_str.lower())
+    else:
+        print(my_str.upper())
 
 #########################################
 
@@ -45,8 +38,8 @@
 
 ##########################################
 
-my_str = "qwer"
-if len(my_str) < 5:
-    print(my_str + my_str[::-1])
-else:
-    print(my_str)
+# my_str = "qwer"
+# if len(my_str) < 5:
+#     print(my_str + my_str[::-1])
+# else:
+#     print(my_str)
